6035d1ab352948eeabe555dc7e4b1ecc6d69be7d
nagi156 <githubsignin156>
2024-10-21 18:32:41 +0900

    prisma導入中

---
+++ script.ts
@@ -0,0 +1,22 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
